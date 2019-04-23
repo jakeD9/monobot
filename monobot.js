f8ef0698d465dc4823f5d9589c339d7dd53b3daa
JakeD <43915365+jakeD9@users.noreply.github.com>
2019-04-23 17:15:15 -0500

    mock and penchenski command added

--- monobot.js
+++ monobot.js
@@ -10,8 +10,48 @@ client.once('ready', () => {
 client.login(process.env.AUTH_TOKEN);
 
 client.on('message', message => {
-    if (!message.content.startsWith(prefix) || message.author.bot) return;
-
-    const args = message.content.slice(prefix.length).split(' ');
+    const args = message.content.slice(prefix.length).split(/ +/);
     const command = args.shift().toLowerCase();
+
+
+    if (!message.content.startsWith(prefix) || message.author.bot) {
+        return
+    } else if (command === 'args-info') {
+        if (!args.length) {
+            return message.channel.send('next time give a command idiot');
+        }
+        message.channel.send(`Command name: ${command}\nArguments: ${args}`);
+
+        // mock command
+        // string -> sTrInG
+    } else if (command === 'mock') {
+        let oldString = args.join(' ');
+        let split = oldString.split('');
+
+        for (let i = 0; i < split.length; i += 2) {
+            split[i] = split[i].toLowerCase();
+        }
+        for (let i = 1; i < split.length; i += 2) {
+            split[i] = split[i].toUpperCase();
+        }
+        let newString = split.join('');
+
+        message.channel.send(newString);
+
+        // penchenski command
+        // string -> sTRING
+    } else if (command === 'penchenski') {
+        let oldString = args.join(' ');
+        let split = oldString.toLowerCase().split(' ');
+
+        for (let i = 0; i < split.length; i++) {
+            split[i] = split[i].charAt(0).toLowerCase() + split[i].substring(1).toUpperCase();
+        }
+
+        let newString = split.join(' ');
+        message.channel.send(newString);
+    }
+
+
+
 });
